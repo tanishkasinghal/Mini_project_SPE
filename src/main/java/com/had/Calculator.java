e44877c7f625f6a2305fe944215da1a060b29d24
tanishkasinghal <tanishka2306@gmail.com>
2023-03-23 14:29:47 +0530

    Ansible

--- src/main/java/com/had/Calculator.java
+++ src/main/java/com/had/Calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 //code
+        //kmdk
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         double number1, number2;
