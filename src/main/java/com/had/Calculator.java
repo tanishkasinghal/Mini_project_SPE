58c1aad814efd70d46ac96711ff5d7d711f6d3c3
tanishkasinghal <tanishka2306@gmail.com>
2023-03-23 14:41:31 +0530

    Ansible

--- src/main/java/com/had/Calculator.java
+++ src/main/java/com/had/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     public Calculator() {
     }
-
+//
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
